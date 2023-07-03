0304098703d4873d1aa2ae6721b44c6c4b4dec6b
Nisal Bandara <nisal.bandara@outlook.com>
2023-07-03 13:12:27 +0530

    deps: use guava for utility functions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     implementation("com.github.javafaker:javafaker:1.0.2")
+    implementation("com.google.guava:guava:32.1.1-jre")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("org.postgresql:postgresql")
