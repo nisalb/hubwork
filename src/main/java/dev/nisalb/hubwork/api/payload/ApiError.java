edab9e3ee961bcfa698ca3309ac5a8a0b2b88493
Nisal Bandara <nisal.bandara@outlook.com>
2023-07-03 23:22:54 +0530

    comp(api): implementation of ApiError

--- src/main/java/dev/nisalb/hubwork/api/payload/ApiError.java
+++ src/main/java/dev/nisalb/hubwork/api/payload/ApiError.java
@@ -1,4 +1,33 @@
 package dev.nisalb.hubwork.api.payload;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Singular;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+
+@Data
+@Builder
 public class ApiError {
+    @Builder.Default
+    private boolean isError = true;
+
+    private HttpStatus code;
+    private String title;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String description;
+
+    @Singular
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private Map<String, Object> properties;
+
+    public static ApiError ok() {
+        return ApiError
+                .builder()
+                .isError(false)
+                .build();
+    }
 }
