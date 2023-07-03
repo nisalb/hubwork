57033ddcb5c67162140b3dcbff74b37579ada1c4
Nisal Bandara <nisal.bandara@outlook.com>
2023-07-03 23:27:46 +0530

    comp(api): implement job CRUD operations in job API controller

--- src/main/java/dev/nisalb/hubwork/api/impl/JobApiController.java
+++ src/main/java/dev/nisalb/hubwork/api/impl/JobApiController.java
@@ -1,65 +1,163 @@
 package dev.nisalb.hubwork.api.impl;
 
 import dev.nisalb.hubwork.api.JobApi;
-import dev.nisalb.hubwork.api.payload.JobInformation;
+import dev.nisalb.hubwork.api.payload.ApiError;
+import dev.nisalb.hubwork.api.payload.JobPayload;
 import dev.nisalb.hubwork.api.payload.ReqeustInformation;
+import dev.nisalb.hubwork.model.Job;
 import dev.nisalb.hubwork.model.JobState;
 import dev.nisalb.hubwork.model.RequestState;
+import dev.nisalb.hubwork.service.JobService;
+import jakarta.validation.Validator;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 public class JobApiController implements JobApi {
 
+    @Setter(onMethod = @__({@Autowired}))
+    private JobService jobService;
+
+    @Setter(onMethod = @__({@Autowired}))
+    private Validator validator;
+
     @Override
-    public ResponseEntity<List<Object>> findAllJobs(Long ownerId, JobState state, Long id) {
-        return ResponseEntity.ok(List.of("findAllJobs"));
+    public ResponseEntity<Set<Job>> findAllJobs(
+            Optional<Long> ownerId,
+            Optional<JobState> state,
+            Optional<Long> workerId
+    ) {
+        Set<Job> result;
+
+        if (ownerId.isEmpty() && state.isEmpty() && workerId.isEmpty()) {
+            result = jobService.findAllJobs();
+        } else {
+            result = jobService.searchJobsBy(ownerId, state, workerId);
+        }
+
+        return ResponseEntity.ok(result);
     }
 
     @Override
-    public ResponseEntity<Object> createJob(JobInformation payload) {
-        return ResponseEntity.ok("createJob");
+    public ResponseEntity<Object> createJob(JobPayload payload) {
+        var validation = validate(payload, "invalid payload to create a job");
+        if (validation.isError()) {
+            return ResponseEntity.badRequest().body(validation);
+        }
+
+        var job = new Job();
+        var result = jobService.createJob(payload, job);
+
+        if (result.isLeft()) {
+            ApiError error = result.getLeft();
+            return buildErrorResponse(error);
+        }
+
+        return ResponseEntity.ok(result.get());
     }
 
     @Override
-    public ResponseEntity<Object> findJob(Long id) {
-        return ResponseEntity.ok("findJob:" + id);
+    public ResponseEntity<Job> findJob(Long id) {
+        var theJob = jobService.findJob(id);
+
+        return theJob
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @Override
     public ResponseEntity<Object> deleteJob(Long id) {
-        return ResponseEntity.ok("deleteJob:" + id);
+        var result = jobService.deleteJob(id);
+
+        if (result.isLeft()) {
+            ApiError error = result.getLeft();
+            return buildErrorResponse(error);
+        }
+
+        return ResponseEntity.ok(result.get());
     }
 
     @Override
-    public ResponseEntity<Object> updateJob(Long id, JobInformation payload) {
-        return ResponseEntity.ok("updateJob:" + id);
+    public ResponseEntity<Object> updateJob(Long id, JobPayload payload) {
+        var validation = validate(payload, "invalid payload to update a job");
+        if (validation.isError()) {
+            return ResponseEntity.badRequest().body(validation);
+        }
+
+        var result = jobService.updateJob(id, payload);
+
+        if (result.isLeft()) {
+            ApiError error = result.getLeft();
+            return buildErrorResponse(error);
+        }
+
+        return ResponseEntity.ok(result.get());
     }
 
     @Override
     public ResponseEntity<Object> makeRequest(Long id, ReqeustInformation payload) {
+        // TODO
         return ResponseEntity.ok("makeRequest:" + id);
     }
 
     @Override
     public ResponseEntity<Object> findRequest(Long jobId, Long reqId) {
+        // TODO
         return ResponseEntity.ok("findRequest:" + jobId + ":" + reqId);
     }
 
     @Override
-    public ResponseEntity<List<Object>> getAllRequestsForAJob(Long id, RequestState state) {
-        return ResponseEntity.ok(List.of("getAllRequstsForAJob:" + id));
+    public ResponseEntity<Set<Object>> getAllRequestsForAJob(Long id, Optional<RequestState> state) {
+        // TODO
+        return ResponseEntity.ok(Set.of("getAllRequstsForAJob:" + id));
     }
 
     @Override
     public ResponseEntity<Object> cancelRequest(Long jobId, Long reqId) {
+        // TODO
         return ResponseEntity.ok("cancelRequest:" + jobId + ":" + reqId);
     }
 
     @Override
     public ResponseEntity<Object> updateRequest(Long jobId, Long reqId, ReqeustInformation payload) {
+        // TODO
         return ResponseEntity.ok("updateRequest:" + jobId + ":" + reqId);
+    }
+
+    private <T> ApiError validate(T payload, String message) {
+        var violations = validator.validate(payload);
+
+        if (violations.isEmpty())
+            return ApiError.ok();
+
+        Set<String> errors = violations.stream()
+                .map(violation -> {
+                    var msg = violation.getMessage();
+                    var value = violation.getInvalidValue();
+                    return msg + " (given = [" + value + "])";
+                })
+                .collect(Collectors.toSet());
+
+        return ApiError.builder()
+                .code(HttpStatus.BAD_REQUEST)
+                .title("INVALID_PAYLOAD")
+                .description(message)
+                .property("violations", errors)
+                .build();
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(ApiError error) {
+        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(error.getCode())) {
+            return ResponseEntity.internalServerError().body(error);
+        }
+
+        return ResponseEntity.badRequest().body(error);
     }
 }
