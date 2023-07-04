88ccc0fdca2637b4f5a0fc5f4070a37095bc60e8
Nisal Bandara <nisal.bandara@outlook.com>
2023-07-04 18:53:41 +0530

    comp(repo): rename JobRepository.searchJobsBy -> searchBy

--- src/main/java/dev/nisalb/hubwork/service/repo/JobRepository.java
+++ src/main/java/dev/nisalb/hubwork/service/repo/JobRepository.java
@@ -12,7 +12,7 @@ public interface JobRepository extends CrudRepository<Job, Long> {
             "where (:owner_id is null or j.owner.id = :owner_id)" +
             "  and (:job_state is null or j.state = :job_state)" +
             "  and (:worker_id is null or (j.worker is not null and j.worker.id = :worker_id))")
-    Iterable<Job> searchJobsBy(
+    Iterable<Job> searchBy(
             @Param("owner_id") Long ownerId,
             @Param("job_state") JobState state,
             @Param("worker_id") Long workerId);
