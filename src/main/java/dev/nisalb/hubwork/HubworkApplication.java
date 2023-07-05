c215246760232c361b37020b09728132cdf79a19
Nisal Bandara <nisal.bandara@outlook.com>
2023-07-05 14:21:27 +0530

    fix: parameterize the pre-populated users and businesses count

--- src/main/java/dev/nisalb/hubwork/HubworkApplication.java
+++ src/main/java/dev/nisalb/hubwork/HubworkApplication.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -18,6 +19,12 @@ public class HubworkApplication {
     @Setter(onMethod = @__({@Autowired}))
     private UserService userService;
 
+    @Value(value = "${app.setup.count.user}")
+    private int numUsersToPopulate = 20;
+
+    @Value(value = "${app.setup.count.business}")
+    private int numBusinessesToPopulate = 5;
+
     private final Logger logger = LoggerFactory.getLogger(HubworkApplication.class);
 
 
@@ -27,8 +34,11 @@ public class HubworkApplication {
 
     @EventListener(ApplicationReadyEvent.class)
     public void populateData() {
-        logger.atInfo().log("Populating Users and Businesses...");
-        userService.initUsersAndBusinesses(20, 5);
+        if (numUsersToPopulate < 1) numUsersToPopulate = 20;
+        if (numBusinessesToPopulate < 1) numBusinessesToPopulate = 5;
+
+        logger.atInfo().log("Populating Users ({}) and Businesses ({})...", numUsersToPopulate, numBusinessesToPopulate);
+        userService.initUsersAndBusinesses(numUsersToPopulate, numBusinessesToPopulate);
         logger.atInfo().log("Users and Businesses are populated");
     }
 
