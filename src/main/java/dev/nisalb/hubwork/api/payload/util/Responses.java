92f95a0d1a52db396f1f13402c164a942dbcfaae
Nisal Bandara <nisal.bandara@outlook.com>
2023-07-05 12:08:54 +0530

    feat(api): add 'created' response type

--- src/main/java/dev/nisalb/hubwork/api/payload/util/Responses.java
+++ src/main/java/dev/nisalb/hubwork/api/payload/util/Responses.java
@@ -3,6 +3,9 @@ package dev.nisalb.hubwork.api.payload.util;
 import dev.nisalb.hubwork.api.payload.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 public class Responses {
 
@@ -16,6 +19,20 @@ public class Responses {
 
     public static <T> ResponseEntity<T> ok(T body) {
         return ResponseEntity.ok(body);
+    }
+
+    public static <T> ResponseEntity<T> created(String url, T body) {
+        String base = ServletUriComponentsBuilder.fromCurrentContextPath().build().toString();
+
+        URI uri = URI.create("http://server.base.url");
+        try {
+            if (!url.startsWith("/"))
+                url = "/" + url;
+            uri = URI.create(base + url);
+        } catch (Exception ignore) {
+        }
+
+        return ResponseEntity.created(uri).body(body);
     }
 
     public static <T> ResponseEntity<T> badRequest(T body) {
